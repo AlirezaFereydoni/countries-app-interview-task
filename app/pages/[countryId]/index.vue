950fdadda1cda642bcccf3997001251935ebcf18
Alireza Fereydoni <Aliirzw@gmail.com>
2025-09-24 20:57:26 +0330

    feat: setup environment with nuxt

---
+++ app/pages/[countryId]/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold bg-sky-500 text-primary">Country {{ countryId }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { countryId } = useRoute().params;
+</script>
