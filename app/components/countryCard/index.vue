b8caf094d82f03f177d36d3bc550c5cfdc478aa5
Alireza Fereydoni <Aliirzw@gmail.com>
2025-09-25 15:45:46 +0330

    feat: country card component

--- app/components/countryCard/index.vue
+++ app/components/countryCard/index.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="w-full box-shadow-md">
-    <NuxtImg :src="country.flags.png" :alt="country.flags.alt" sizes="50px" class="w-full h-auto" />
+  <NuxtLink :to="`/${country.name.official.toLowerCase()}`">
+    <div class="w-full shadow-md rounded-lg overflow-hidden country-card">
+      <NuxtImg
+        :src="country.flags.png"
+        :alt="country.flags.alt"
+        loading="lazy"
+        sizes="50px"
+        class="w-full h-50 shadow-xs"
+      />
 
-    <div class="flex flex-col gap-2 px-5 py-10">
-      <h2 class="mb-2 font-extrabold">{{ country.name.common }}</h2>
+      <div class="flex flex-col gap-2 px-8 py-10">
+        <h2 class="mb-2 font-extrabold">{{ country.name.common }}</h2>
 
-      <DescriptionLine label="Population" :value="country.population" />
-      <DescriptionLine label="Region" :value="country.region" />
-      <DescriptionLine label="Capital" :value="country.capital.join(', ')" />
+        <TitleValue title="Population" :value="formatNumberWithCommas(country.population)" />
+        <TitleValue title="Region" :value="country.region" />
+        <TitleValue title="Capital" :value="country.capital.join(', ')" />
+      </div>
     </div>
-  </div>
+  </NuxtLink>
 </template>
 
 <script setup lang="ts">
 import type { Country } from '~/types';
-import DescriptionLine from './DescriptionLine.vue';
+import TitleValue from '../atomic/TitleValue.vue';
 
-const props = defineProps<{
+defineProps<{
   country: Country;
 }>();
 </script>
+
+<style scoped>
+.country-card {
+  background-color: var(--bg-secondary);
+}
+</style>
