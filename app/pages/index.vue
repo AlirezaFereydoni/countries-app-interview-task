9e59638456e648c6553ee35ad49f6751d6610859
Alireza Fereydoni <Aliirzw@gmail.com>
2025-09-25 15:46:55 +0330

    feat: country list page basic version

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,11 +17,7 @@ API.get('/all?fields=name,population,region,capital,flags')
 
 <template>
   <div class="w-full">
-    <h1>Countries</h1>
-
-    <div
-      class="p-10 md:p-15 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-10 lg:gap-15"
-    >
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-20 gap-y-15">
       <CountryCard v-for="country in countries" :key="country.name.common" :country="country" />
     </div>
   </div>
