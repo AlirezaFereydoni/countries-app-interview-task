69c31ea2afb4e3a0fd93a150cc59e6ca384e42d0
Alireza Fereydoni <Aliirzw@gmail.com>
2025-09-25 23:13:47 +0330

    feat: loading and error component

---
+++ app/components/atomic/Error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center items-center min-h-screen h-full">
+    <p class="text-red-500 text-center">⚠️ {{ message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  message: string;
+}>();
+</script>
---
+++ app/components/atomic/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center min-h-screen h-full">
+    <p class="text-center mx-auto">Loading...</p>
+  </div>
+</template>
