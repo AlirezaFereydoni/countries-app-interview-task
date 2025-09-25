1b75ce2f27980edc59a21097ed202792c07d7dae
Alireza Fereydoni <Aliirzw@gmail.com>
2025-09-25 15:43:21 +0330

    feat: atomic titleValue component

---
+++ app/components/atomic/TitleValue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex gap-1 items-center">
+    <p class="font-semibold">{{ title }}:</p>
+    <span>{{ value }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  value: string | number;
+}>();
+</script>
