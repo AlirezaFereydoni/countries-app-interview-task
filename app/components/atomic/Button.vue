93d78e4c3a8bf837798f05333f0529851fde3aa5
Alireza Fereydoni <Aliirzw@gmail.com>
2025-09-25 15:57:41 +0330

    feat: button component

---
+++ app/components/atomic/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    @click="onClick"
+    class="px-4 py-2 flex items-center gap-2 rounded shadow-sm cursor-pointer button"
+  >
+    <Icon v-if="icon" :name="icon" class="w-4 h-4" />
+    <p>{{ label }}</p>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string;
+  onClick: () => void;
+  icon?: string;
+}>();
+</script>
+
+<style scoped>
+.button {
+  background-color: var(--bg-secondary);
+}
+</style>
