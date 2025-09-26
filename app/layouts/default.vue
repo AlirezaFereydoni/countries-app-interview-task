b5f25986a88d6896b5cb7e0cbc21862a8d9f833c
Alireza Fereydoni <Aliirzw@gmail.com>
2025-09-26 11:57:57 +0330

    refactor: remove inline styles

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="min-h-screen" style="background-color: var(--bg-primary)">
-    <header class="shadow-sm" style="background-color: var(--bg-secondary)">
-      <div class="container mx-auto px-4 py-6">
+  <div class="min-h-screen container-wrapper">
+    <header class="shadow-sm header-wrapper">
+      <div class="container mx-auto px-6 py-6">
         <div class="flex items-center justify-between">
-          <h1 class="text-sm md:text-xl font-extrabold" style="color: var(--text-primary)">
-            Where in the world?
-          </h1>
+          <h1 class="text-sm md:text-xl font-extrabold header-title">Where in the world?</h1>
           <button
             @click="toggleTheme"
-            class="flex items-center gap-2 hover:opacity-80 transition-opacity cursor-pointer"
-            style="color: var(--text-primary)"
+            class="flex items-center gap-2 hover:opacity-80 transition-opacity cursor-pointer switch-theme-button"
           >
             <Icon :name="isDark ? 'heroicons:sun' : 'heroicons:moon'" class="w-5 h-5" />
             <span class="font-semibold text-sm md:text-lg">
@@ -29,3 +26,18 @@
 <script setup lang="ts">
 const { isDark, toggleTheme } = useTheme();
 </script>
+
+<style scoped>
+.container-wrapper {
+  background-color: var(--bg-primary);
+}
+.header-wrapper {
+  background-color: var(--bg-secondary);
+}
+.header-title {
+  color: var(--text-primary);
+}
+.switch-theme-button {
+  color: var(--text-primary);
+}
+</style>
